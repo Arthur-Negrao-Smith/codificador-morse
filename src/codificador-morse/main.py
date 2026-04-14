f8fd8a6259e019b406d4e75fac53f517ed04dfd2
Arthur Negrão <arthurnegraosmith@gmail.com>
2026-04-14 12:17:42 -0300

    feat: Adicionado o QT para criar uma interface gráfica

--- src/codificador-morse/main.py
+++ src/codificador-morse/main.py
@@ -1,170 +1,115 @@
-import sounddevice as sd
-import numpy as np
+import sys
+import threading
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QTextEdit,
+    QPushButton,
+    QMessageBox,
+)
 
-# The dict to translate the code
-MORSE_CODE_DICT: dict[str, str] = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----",
-    ", ": "--..--",
-    ".": ".-.-.-",
-    "?": "..--..",
-    "/": "-..-.",
-    "-": "-....-",
-    "(": "-.--.",
-    ")": "-.--.-",
-    " ": " ",
-}
+from .encode.encoder import encode_message, play_encoded_message
 
 
-def encode_char(char: str, slots_between_dots: int = 1) -> str:
-    """
-    Encode a ASCII char to morse code.
+class MorseCodeApp(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Codificador de Código Morse")
+        self.resize(500, 350)
+        self.setup_ui()
 
-    Args:
-        char (str): A char to encode.
-        slots_between_dots (int): Dot spaces between each morse symbol: "-" or ".".
+    def setup_ui(self) -> None:
+        """
+        Default ui setup.
+        """
+        layout = QVBoxLayout()
 
-    Returns:
-        str: Return the char encoded.
-    """
-    # translate from ASCII char to morse code
-    raw_encoded: str = MORSE_CODE_DICT[char]
-    finished_encoded: str = ""
+        # input
+        self.input_label = QLabel("Digite o texto (Caracteres ASCII suportados):")
+        layout.addWidget(self.input_label)
 
-    for i, current_char_morse in enumerate(raw_encoded):
-        finished_encoded += current_char_morse
-        # The word do not has a blank space in finish
-        if i != len(raw_encoded) - 1:
-            finished_encoded += " " * slots_between_dots
+        self.input_field = QLineEdit()
+        self.input_field.setPlaceholderText("Ex: SOS")
+        layout.addWidget(self.input_field)
 
-    return finished_encoded
+        # encode button
+        self.btn_encode = QPushButton("Codificar para Morse")
+        self.btn_encode.clicked.connect(self.handle_encode)
+        layout.addWidget(self.btn_encode)
 
+        # output
+        self.output_label = QLabel("Código Morse Gerado:")
+        layout.addWidget(self.output_label)
 
-def encode_message(
-    message: str,
-    time_stemp_between_words: int = 7,
-    time_stemp_between_chars: int = 3,
-) -> str | None:
-    """
-    Encode a raw message to morse code.
+        self.output_field = QTextEdit()
+        self.output_field.setReadOnly(True)
+        self.output_field.setPlaceholderText("O resultado aparecerá aqui...")
+        layout.addWidget(self.output_field)
 
-    Args:
-        message (str): Message to encode.
-        time_stemp_between_words (int): Dot spaces between each word,
-        time_stemp_between_chars (int): Dot spaces between each ASCII character.
+        # audio button
+        self.btn_play = QPushButton("Tocar Som")
+        self.btn_play.clicked.connect(self.handle_play)
+        self.btn_play.setEnabled(False)  # Disabled until has a valid code
+        layout.addWidget(self.btn_play)
 
-    Returns:
-        str: The message enconded.
-        None: If the message has a invalid character.
-    """
-    raw_msg: str = message.strip().upper()
+        self.setLayout(layout)
 
-    try:
-        words_list: list[str] = raw_msg.split(" ")
-        encoded_words_list: list[str] = []
-        for word in words_list:
-            encoded_words_list.append(
-                (" " * time_stemp_between_chars).join(map(encode_char, word))
+    def handle_encode(self) -> None:
+        """
+        Handle the text input to show the encoded message.
+        """
+        raw_msg = self.input_field.text()
+        if not raw_msg:
+            QMessageBox.warning(
+                self, "Aviso", "Por favor, digite um texto para codificar."
             )
+            return
 
-        encoded_msg: str = (
-            (" " * time_stemp_between_words).join(encoded_words_list).strip()
-        )
-        return encoded_msg
-    except:
-        print(f"O texto '{raw_msg}' é inválido. Use apenas caracteres ASCII.")
-        return None
+        encoded_msg = encode_message(raw_msg)
 
-
-def play_tone(frequency: float | int, duration: float, sample_rate=44100):
-    """
-    Generates and plays a simple tone.
-
-    Args:
-        frequency (int | float): Tone frequency (Hz).
-        duration (float): Durantion to play tone (ms).
-        sample_rate (int): Number of samples to play.
-    """
-    # Generate time points
-    time = np.linspace(0.0, duration, int(sample_rate * duration), endpoint=False)
-
-    # Generate a sine wave
-    data = np.sin(2.0 * np.pi * frequency * time)
-
-    # Play the data (data is automatically treated as float32)
-    sd.play(data, samplerate=sample_rate)
-
-    # Wait until playback is finished
-    sd.wait()
-
-
-def play_encoded_message(
-    msg: str, dot_duration: float = 0.06, frequency: int = 440
-) -> None:
-    """
-    Play a entire encoded text.
-
-    Args:
-        msg (str): A message to encond into a morse code and play.
-        dot_duration (float): Time stemp to represent a duration a "." in morse code.
-    """
-    for char in msg:
-        if char == ".":
-            # 1 dot duration to represent a "."
-            play_tone(frequency, dot_duration)
-        elif char == "-":
-            # 3 times the dot duration to represent a "-"
-            play_tone(frequency, dot_duration * 3)
+        if encoded_msg:
+            self.output_field.setPlainText(encoded_msg)
+            self.btn_play.setEnabled(True)
         else:
-            # to represent a blank space
-            sd.sleep(int(dot_duration * 1000))
+            QMessageBox.critical(
+                self,
+                "Erro",
+                "Texto contém caracteres não suportados pelo dicionário Morse.",
+            )
+            self.output_field.clear()
+            self.btn_play.setEnabled(False)
+
+    def handle_play(self) -> None:
+        """
+        Play the encoded message in audio interface with a new Thread.
+        """
+        msg = self.output_field.toPlainText()
+        if msg:
+            # deactive the button to avoid multiple executions
+            self.btn_play.setEnabled(False)
+            self.btn_play.setText("Tocando...")
+
+            # use a thread to avoid ui blocking
+            threading.Thread(
+                target=self.play_audio_thread, args=(msg,), daemon=True
+            ).start()
+
+    def play_audio_thread(self, msg: str) -> None:
+        """
+        Play the message a enable the audio button.
+        """
+        try:
+            play_encoded_message(msg)
+        finally:
+            self.btn_play.setEnabled(True)
+            self.btn_play.setText("Tocar Som")
 
 
-while True:
-    raw_msg: str = input("Digite seu texto para codificar: ").strip().upper()
-
-    if raw_msg == "":
-        break
-
-    msg = encode_message(raw_msg)
-
-    if msg is not None:
-        print(f"A mensagem codificada é: {msg}")
-        print("Tocando a mensagem codificada...")
-        play_encoded_message(msg)
-
-    print("-" * 50)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MorseCodeApp()
+    window.show()
+    sys.exit(app.exec())
